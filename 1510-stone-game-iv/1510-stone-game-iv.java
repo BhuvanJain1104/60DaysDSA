d5f292921f6933dfb46feddc0c02afcab9cbc276
BhuvanJain1104 <jain.bhuvancr7@gmail.com>
2026-08-10 20:09:30 +0530

    Time: 38 ms (48.82%), Space: 53.8 MB (13.84%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,18 @@
+class Solution {
+   Boolean[] dp = new Boolean[100001];    
+   public boolean winnerSquareGame(int n) {
+        if(n==0)
+            return false;
+
+        if(dp[n] != null )
+            return dp[n];
+        for(int i=1;i*i<=n;i++)
+        {
+            if(!winnerSquareGame(n-i*i)){
+                return dp[n]=true;
+            }
+        }
+        return dp[n]=false;
+        
+    }
+}
